fcfba9a6a3686828b3e41f0c3849d5a6cc34b877
mrzaid <zaidahmed87861@gmail.com>
2020-06-01 02:46:59 +0500

    advance javamosh

---
+++ hardJava/app.js
@@ -0,0 +1,11 @@
+// const person= {
+//     name:"zaid",
+//     walk: function(x){
+//         console.log(`function in object is method ${x} `)
+//            },
+//     //talk(x) {console.log(`short es6 ${x} `)} //es6 feature
+//     talk(x) {console.log(`short es6 ${x} `)}      
+// }
+//person.walk("kaam pachis");
+//person.talk("kaam double pachis")
+//THIS    
---
+++ hardJava/lambda function.js
@@ -0,0 +1,15 @@
+// let num =[2,3,4,5,6,7];
+// num.map((value)=>{
+// return(
+//     console.log(`hello ${value}`),
+//     x = value*value
+// );
+// }
+// )
+// console.log(x)
+function setup () {
+ createCanvas(600,400);
+ background(0);
+ let button = createButton('press');
+ button.mousePressed();
+}
